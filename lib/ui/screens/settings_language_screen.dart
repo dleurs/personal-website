5faf7ec9b40efbe320243c2e985325223707607f
Dimitri Leurs <dimitri.leurs@numberly.com>
2020-12-05 21:52:27 +0100

    Bug removed

--- lib/ui/screens/settings_language_screen.dart
+++ lib/ui/screens/settings_language_screen.dart
@@ -83,30 +83,28 @@ class _SettingsLanguageScreenState extends State<SettingsLanguageScreen> {
             ),
             SizedBox(
               width: 250,
-              child: Flexible(
-                child: RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      S.load(Locale("ru"));
-                    });
-                    (Router.of(context).routerDelegate as MyRouterDelegate)
-                        .toHomeScreen();
-                  },
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset('assets/images/ru-flag.jpg',
-                          height: 20, width: 35),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text("На русском",
-                            style: Theme.of(context).textTheme.headline5),
-                      ),
-                    ],
-                  ),
+              child: RaisedButton(
+                onPressed: () {
+                  setState(() {
+                    S.load(Locale("ru"));
+                  });
+                  (Router.of(context).routerDelegate as MyRouterDelegate)
+                      .toHomeScreen();
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset('assets/images/ru-flag.jpg',
+                        height: 20, width: 35),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text("На русском",
+                          style: Theme.of(context).textTheme.headline5),
+                    ),
+                  ],
                 ),
               ),
             )
